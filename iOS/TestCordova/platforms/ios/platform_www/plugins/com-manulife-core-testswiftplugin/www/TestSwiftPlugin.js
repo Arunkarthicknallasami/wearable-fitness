aeb37b0b916a2b34f68915a9f22c3952d3914b72
jeffrey garcia wong <jeffrey_garcia_wong@manulife.com>
2017-04-18 09:46:29 +0800

    remove the cordova test app from wearable fitness project and move it under cloud native

--- iOS/TestCordova/platforms/ios/platform_www/plugins/com-manulife-core-testswiftplugin/www/TestSwiftPlugin.js
+++
@@ -1,8 +0,0 @@
-cordova.define("com-manulife-core-testswiftplugin.TestSwiftPlugin", function(require, exports, module) {
-var exec = require('cordova/exec');
-
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "TestSwiftPlugin", "coolMethod", [arg0]);
-};
-
-});
